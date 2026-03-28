5852a62b5a97ab709f993b5a1ca5afe0b3f6f687
Andrew Clemons <andrew.clemons@gmail.com>
2026-03-28 18:58:21 +0900

    Switch project to uv, default tooling to python3.
    We don't yet fully support python3, but to make development simpler,
    we'll default the tooling to Python 3.9 (which is python version
    included in stable - 15.0).
    
    We'll switch the tests to run in docker, but now run them in a real
    slackware container.
    
    This also updates outdated GHA and pre-commit dependencies.

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,7 +9,7 @@ def test_versions_match():
     """Checks if the version in pyproject.toml and slackroll_version in `slackroll` match."""
 
     pyproject_file = os.path.join(os.path.dirname(__file__), "..", "pyproject.toml")
-    pyproject_version = toml.loads(open(str(pyproject_file)).read())["tool"]["poetry"][
+    pyproject_version = toml.loads(open(str(pyproject_file)).read())["project"][
         "version"
     ]
 
