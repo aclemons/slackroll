f731017c2b80f74d06167d717b61e9f15ca7bdad
Andrew Clemons <andrew.clemons@gmail.com>
2026-04-14 21:46:02 +0900

    Introduce basic test coverage of `analyze_changes`.

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,9 @@ except ImportError:
     TYPE_CHECKING = False
 
 if TYPE_CHECKING:
-    from typing import Any, List
+    from typing import Any, Dict, List, Optional
+
+    from slackroll import SlackwarePackage
 
 PY2 = sys.version_info[0] == 2
 
@@ -35,6 +37,41 @@ class FakeStream(object):
     def getvalue(self):
         # type: () -> str
         return "".join(self.messages)
+
+
+class PersistentList(object):
+    def __init__(self, initial=None):
+        # type: (Optional[Dict[str, int]]) -> None
+        self._data = initial or {}  # type: Dict[str, int]
+        self.sync_calls = 0
+
+    def sync(self):
+        # type: () -> None
+        self.sync_calls += 1
+
+    def __contains__(self, key):
+        # type: (str) -> bool
+        return key in self._data
+
+    def __getitem__(self, key):
+        # type: (str) -> int
+        return self._data[key]
+
+    def __setitem__(self, key, value):
+        # type: (str, int) -> None
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        # type: (str) -> None
+        del self._data[key]
+
+    def keys(self):
+        # type: () -> List[str]
+        return list(self._data.keys())
+
+    def __eq__(self, other):
+        # type: (object) -> bool
+        return self._data == other
 
 
 def start_patch(request, target, *args, **kwargs):
@@ -89,3 +126,15 @@ def bytes_literal(text):
 def decode_bytes_literal(text, encoding):
     # type: (str, str) -> Any
     return bytes_literal(text).decode(encoding)
+
+
+def persistent_dict(persistent_list):
+    # type: (PersistentList) -> Dict[str, int]
+    return persistent_list  # type: ignore
+
+
+def build_pkg(name, version, path):
+    # type: (str, str, str) -> SlackwarePackage
+    from slackroll import SlackwarePackage
+
+    return SlackwarePackage(name, version, "x86_64", "1", path, ".txz", None, None)
